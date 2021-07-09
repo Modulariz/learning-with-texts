054755e6ea55e2c235e6feefb5207d7ab87f0a62
Vagus-art <agustin.favo@gmail.com>
2021-07-08 21:16:05 -0300

    change title

--- login.php
+++ login.php
@@ -11,7 +11,7 @@
 <html lang = "en">
    
    <head>
-      <title>Tutorialspoint.com</title>
+      <title>LWT Login Page</title>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
       
       <style>
